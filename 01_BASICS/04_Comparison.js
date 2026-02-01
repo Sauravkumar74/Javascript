dec3eef36ce91a0d82987aff8a44cef8d5ca5d7b
Sauravkumar74 <iamsauravkumar74@gmail.com>
2026-02-01 22:10:49 +0530

    Added new file 04_Comparison.js

---
+++ 01_BASICS/04_Comparison.js
@@ -0,0 +1,20 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// avoid these types of comparison
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+// === this is called strict check
+// console.log("2" == 2); => true btaega kyuki conversion kar raha hai isme "2" ko number mai convert kar dera hai
+
+// console.log("2" === 2) => false btaega kyuki yaha conversion nahi kr raha datatype bhi check karega 
+
+
